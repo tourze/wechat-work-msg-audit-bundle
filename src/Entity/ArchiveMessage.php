6b6486596b17b868b8dde39c45608186c1adb371
github-ci <you@example.com>
2025-04-28 09:44:29 +0000

    fix: 修复字段错误

--- src/Entity/ArchiveMessage.php
+++ src/Entity/ArchiveMessage.php
@@ -24,7 +24,7 @@ class ArchiveMessage
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ORM\Column(type: Types::JSON, nullable: true, options: ['comment' => '上下文'])]
     private ?array $context = [];
