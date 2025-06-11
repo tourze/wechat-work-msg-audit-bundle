39f5acbcfae970c22e2f579f21daa592b8e9245d
github-ci <you@example.com>
2025-06-12 05:53:49 +0800

    fix catch

--- tests/DependencyInjection/WechatWorkMsgAuditExtensionTest.php
+++ tests/DependencyInjection/WechatWorkMsgAuditExtensionTest.php
@@ -83,7 +83,7 @@ class WechatWorkMsgAuditExtensionTest extends TestCase
         try {
             $this->container->compile();
             $this->assertTrue(true, 'Container compilation should succeed');
-        } catch  (\Throwable $e) {
+        } catch (\Throwable $e) {
             $this->fail('Container compilation failed: ' . $e->getMessage());
         }
     }
