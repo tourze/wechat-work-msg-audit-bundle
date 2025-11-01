efb2591d197d463c7554c039b587a87da56962ec
github-ci <you@example.com>
2025-11-02 03:33:39 +0800

    update++++++

--- tests/Request/GetPermitUserListRequestTest.php
+++ tests/Request/GetPermitUserListRequestTest.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace WechatWorkMsgAuditBundle\Tests\Request;
 
-use HttpClientBundle\Tests\Request\RequestTestCase;
+use HttpClientBundle\Test\RequestTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 use WechatWorkMsgAuditBundle\Request\GetPermitUserListRequest;
 
 /**
